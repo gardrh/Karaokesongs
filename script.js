334ebcc5f2624722190d4ec7af2e335d42febc29
gardrh <gardrh@gmail.com>
2023-03-08 22:20:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,14 @@ generateBtn.addEventListener("click", () => {
 			resultElement.innerHTML = "The CSV file has " + rowCount + " rows.";
 		};
 		request.send();
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
