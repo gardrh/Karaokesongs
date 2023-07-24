0f62c5455e16d1313f9a78123c600830e5f3e77f
gardrh <gardrh@gmail.com>
2023-07-24 12:56:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -48,5 +48,5 @@ function selectSong() {
   const randomIndex = Math.floor(Math.random() * filteredData.length);
   const randomSong = filteredData[randomIndex];
 
-  songResult.innerHTML = `You should sing: <b>${randomSong.Artist}</b> by <b>${randomSong.Title}</b>`;
+  songResult.innerHTML = `You should sing: <b>${randomSong.Artist}</b> by <i><b>${randomSong.Title}</b></i>`;
 }
