f6f1cedda42ba9a673cd033d8fec1e10e4b0ca60
gardrh <gardrh@gmail.com>
2023-07-30 16:00:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,9 @@
 const selectSongBtn = document.getElementById('select-song-btn');
 const songResult = document.getElementById('song-result');
 const genreBtns = document.querySelectorAll('.genre-btn');
+const data = [];
 
-let data = [];
-
-fetch('data.csv')
-  .then((response) => response.text())
-  .then((csv) => {
-    const rows = csv.trim().split('\n');
-
-    for (let i = 1; i < rows.length; i++) {
-      const row = rows[i].split(',');
-      data.push({
-        Artist: row[0],
-        Title: row[1],
-        Type: row[2],
-      });
-    }
-
-    // Call selectSong() to display a random song on page load
-    selectSong();
-  });
-
-genreBtns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    // Remove active class from all buttons
-    genreBtns.forEach((btn) => btn.classList.remove('active'));
-
-    // Add active class to the clicked button
-    btn.classList.add('active');
-
-    selectSong();
-  });
-});
-
-selectSongBtn.addEventListener('click', selectSong);
-
+// Function to select a random song and display it
 function selectSong() {
   const selectedType = document.querySelector('.genre-btn.active').textContent;
 
@@ -50,3 +18,53 @@ function selectSong() {
 
   songResult.innerHTML = `You should sing: <b>${randomSong.Artist}</b> by <i><b>${randomSong.Title}</b></i>`;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Load the CSV file and store the data
+  fetch('data.csv')
+    .then((response) => response.text())
+    .then((csv) => {
+      const rows = csv.trim().split('\n');
+
+      for (let i = 1; i < rows.length; i++) {
+        const row = rows[i].split(',');
+        data.push({
+          Artist: row[0],
+          Title: row[1],
+          Type: row[2],
+        });
+      }
+
+      // Call selectSong() to display a random song on page load
+      selectSong();
+    });
+
+  // Get the genre buttons and the "SING!" button
+  const genreBtns = document.querySelectorAll('.genre-btn');
+  const singBtn = document.getElementById('sing-btn');
+
+  // Add click event listeners to genre buttons
+  genreBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      // Remove active class from all buttons
+      genreBtns.forEach((btn) => btn.classList.remove('active'));
+
+      // Add active class to the clicked button
+      btn.classList.add('active');
+
+      // Scroll to the "SING!" button
+      const singBtnPosition = singBtn.getBoundingClientRect().top;
+      const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+      const targetScrollPosition = currentScrollPosition + singBtnPosition;
+      window.scrollTo({
+        top: targetScrollPosition,
+        behavior: 'smooth',
+      });
+
+      selectSong();
+    });
+  });
+
+  // Click the "SING!" button to select a random song
+  selectSongBtn.addEventListener('click', selectSong);
+});
